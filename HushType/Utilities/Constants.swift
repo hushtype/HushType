bfc002f9a15beec906a0b45ee7088a8abe0ae4bf
harungungorer <harungungorer@gmail.com>
2026-02-19 22:06:49 +0300

    fix: wire overlay and templates, remove dead code

--- HushType/Utilities/Constants.swift
+++ HushType/Utilities/Constants.swift
@@ -37,7 +37,7 @@ enum Constants {
 
     /// Remote model registry
     enum Registry {
-        static let manifestURL = URL(string: "https://raw.githubusercontent.com/harungungorer/HushType/main/registry/models.json")!
+        static let manifestURL = URL(string: "https://raw.githubusercontent.com/hushtype/HushType/main/registry/models.json")!
         static let refreshIntervalSeconds: TimeInterval = 24 * 60 * 60 // 24 hours
     }
 
